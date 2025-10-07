c339c50333b7c9d5dca3f2359853f4eac8e13142
subugee <sasubramanian_md@hotmail.com>
2025-10-07 10:16:14 +0530

    core routing

---
+++ core-routing/src/app/dashboard/settings/password/page.js
@@ -0,0 +1,3 @@
+export default function PasswordPage(){
+    return <h1>Password Page</h1>
+}
---
+++ core-routing/src/app/products/page.js
@@ -0,0 +1,6 @@
+
+export default function ProductPage(){
+     return <h1>
+        This is Product Page
+     </h1>
+}
